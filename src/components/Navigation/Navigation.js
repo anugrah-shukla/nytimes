491346ff99678a3338c5639816ce4c6cf4636c99
Muskan Chitara <Muskan Chitara>
2020-05-09 02:28:59 +0530

    random message

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './style.css';
+import nytimes_logo from './nytimes_logo.png';
 
 class Navigation extends React.Component {
     render() {
       return (
       <div>
         <div className="logo">
-          <img src="..\public\nytimes_logo.png" alt="This is an image."/>
+          <img src={nytimes_logo} alt="This is an image."/>
         </div>
         <div className="list">
           <p>Dashboard</p>
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './style.css';
+import alt_img from './alt_img.png';
 
 class Results extends React.Component {
     render() {
-      return (<div>
-          <div className="text">
-              Search for breaking news from across the world, across the times.
-          </div>
-          <div className="image">
-              <img src="..\public\undraw_treasure_of9i.svg" alt="This is an image."/>
-          </div>
-          </div>
+      return (<div className="div1">
+                <p className="text">
+                    Search for breaking news from across the world, across the times.
+                </p>
+                <img src={alt_img} alt="This is an image." className="image"/>
+              </div>
       );
     }
 }
